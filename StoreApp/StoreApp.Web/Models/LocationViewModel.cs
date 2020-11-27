36946f496114e901afc112f4b48ac7f37f2758b6
Oladipo Osinubi <diposinubi@gmail.com>
2020-11-27 18:21:12 -0500

    can create new inventory

---
+++ StoreApp/StoreApp.Web/Models/LocationViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using StoreApp.Library;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace StoreApp.Web.Models
+{
+    public class LocationViewModel
+    {
+        public int LocationId { get; set; }
+
+        [Display(Name = "Location Name")]
+        [Required, RegularExpression("[A-Z].*")]
+        public string Name { get; set; }
+
+        [Display(Name = "Location Address")]
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "City")]
+        [Required]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        [Required, MaxLength(2)]
+        public string State { get; set; }
+
+        public List<Inventory> Inventory { get; set; }
+    }
+}
