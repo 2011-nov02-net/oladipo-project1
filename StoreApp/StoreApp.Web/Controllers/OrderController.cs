b627f620ff1d3fb19617d8450ec8cb85eed16567
Oladipo Osinubi <diposinubi@gmail.com>
2020-11-27 12:43:52 -0500

    getting orders by location

---
+++ StoreApp/StoreApp.Web/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.DataModel.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StoreApp.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private StoreAppRepository _storeRepo;
+
+        public OrderController(StoreAppRepository storeRepo)
+        {
+            _storeRepo = storeRepo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index(int id)
+        {
+            if (id < 0)
+            {
+                return NotFound();
+            }
+
+            var inventory = _storeRepo.GetOrdersByLocationId(id);
+
+            return View(inventory);
+        }
+    }
+}
